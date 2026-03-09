d51fd677e13a50aeaee6bd6017046cd4704d2d52
Joshua Park <joshuapark823@gmail.com>
2026-03-08 21:27:11 -0400

    cleanup: remove lint silencing

--- src/hooks/usePokemonInfinite.ts
+++ src/hooks/usePokemonInfinite.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { useInfiniteQuery } from '@tanstack/react-query'
 
 import { fetchPokemon } from '@/lib/api/pokemon'
